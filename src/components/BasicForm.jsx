8889dba198c0d20a2168351f35e22a69dc350e39
ValeryNunezBrow <nunez.ramirez.valery.aylin@gmail.com>
2025-07-15 15:22:38 -0600

    Validacion finalizada

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -1,7 +1,21 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+
+const schema = yup.object({
+  username: yup.string().required('The name is required'),
+  password: yup.string().min(6, 'The created password must have at least 6 characters.'),
+  confirmPassword: yup.string().oneOf([yup.ref('password')], 'The passwords do not match').required('Confirm your password')
+})
 
 export const BasicForm = () => {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   const onSubmit = (data) => {
     console.log(data)
@@ -9,7 +23,12 @@ export const BasicForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('username')} placeholder='Usuario' />
+      <input type='text' {...register('username')} placeholder='Usurename' />
+      {errors.username && <p>{errors.username.message}</p>}
+      <input type='password' {...register('password')} placeholder='Password' />
+      {errors.password && <p>{errors.password.message}</p>}
+      <input type='password' {...register('confirmPassword')} placeholder='Confirm password' />
+      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
       <button type='submit'>Enviar</button>
     </form>
   )
